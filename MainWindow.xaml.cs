3150a30fe254ea3a0aaadb1f7480bbea351344c9
Maciej Zieliński <maciej.zielinski19@gmail.com>
2023-02-02 10:09:45 +0100

    MatchingGameFinished

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,16 +15,14 @@ using System.Windows.Shapes;
 
 namespace MatchingGame
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
 
-            SetUpGame();
+            SetUpGame(); 
         }
 
         private void SetUpGame()
@@ -51,6 +49,29 @@ namespace MatchingGame
             } 
             
             
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false; 
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden; 
+                findingMatch = false;
+            } 
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
         }
     }
 }
